306bfb0009592a61eb93fc5a2a2f71c055afad10
rhude <jeff@rhude.com>
2017-12-18 12:34:27 -0600

    Now bulk pulling from s3 via keeval and rendering templates.

--- helm-build.py
+++ helm-build.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 
-from keeval.keeval import S3ConfigStore
-import jinja2
-from jinja2 import meta
-import os
+
 import sys
+from keeval.keeval import S3ConfigStore
+from os import getcwd
+import jinja2
 import fnmatch
 import base64
 import argparse
-
-
-cwd = os.getcwd()
-all_discovered_templates = []
-
-import os
 import collections
 import pprint
+import datetime
 
 
 class ConfigGenerator(object):
     """ Generates a dictionary of file system objects and their parents based off a
-    given list of paths.
+    given list of keys.
+
+    Uses keeval to fetch keys from s3.
 
     Conflicts will be taken from left to right, the rightmost (last) config winning.
 
@@ -29,8 +26,15 @@ class ConfigGenerator(object):
 
     """
 
-    def __init__(self, *paths):
+    def __init__(self, aws_profile, keeval_bucket, *paths):
+        # type: (object, object, object) -> object
         self.paths = paths
+        # Create the s3 configstore instance from keeval.
+        self.store = S3ConfigStore(aws_profile, keeval_bucket)
+
+    @property
+    def _delimiter(self):
+        return '.'
 
     def generate(self):
         configs = [self.generate_config(path) for path in self.paths]
@@ -42,32 +46,20 @@ class ConfigGenerator(object):
             self.dict_merge(result, config)
         return result
 
-    def generate_config(self, rootdir):
-        data = {}
-
-        rootdir = rootdir.rstrip(os.sep) + os.sep
-        start = rootdir.rfind(os.sep) + 1
-
-        for path, dirs, files in os.walk(rootdir):
-
-            folders = path[start:].split(os.sep)
-            subdir = dict.fromkeys(files)
-
-            for k in subdir.keys():
-                subdir[k] = self._get_file_value(os.path.join(path, k))
-
-            parent = reduce(dict.get, folders[:-1], data)
-            parent[folders[-1]] = subdir
-            pp = pprint.PrettyPrinter()
-            pp.pprint(data)
-        return data
-
-    def _get_file_value(self, path):
-        with open(path) as f:
-            value = f.read().strip()
+    def generate_config(self, path):
+        path = path + '/'
+        nested_dict = {}
+        key_list = self.store.list(path)
+        key_data = self.store.read_bulk(key_list)
+        for key in key_data.keys():
+            path_delim = path.replace('/', self._delimiter)
+            new_key = key.split(path_delim)[1]
+            value = key_data[key].strip()
             if value.isdigit():
                 value = int(value)
-        return value
+            new_dict = reduce(lambda res, cur: {cur: res}, reversed(new_key.split(self._delimiter)), value)
+            self.dict_merge(nested_dict, new_dict)
+        return nested_dict
 
     @staticmethod
     def dict_merge(dct, merge_dct):
@@ -89,9 +81,9 @@ class ConfigGenerator(object):
 
 
 
-class BuildSecrets(object):
+class j2Builder(object):
     def __init__(self, keeval_bucket):
-        self.name = 'BuildSecrets'
+        self.name = 'j2Builder'
         self.keeval_bucket = keeval_bucket
 
     def base64encode(self, input):
@@ -106,37 +98,14 @@ class BuildSecrets(object):
         j2env.filters['base64encode'] = self.base64encode
         return j2env.get_template(filename).render(context)
 
-
-    def get_secret(self, key):
-        print "Getting: " + self.keeval_bucket + '/' + key
-        kv = S3ConfigStore(profile='development', bucket_name=self.keeval_bucket)
-        data = kv.read(key)
-        return data
-
     def write_template(self, template_filename, rendered_template):
         # Remove the j2 extension.
         helm_secret_file = os.path.splitext(template_filename)[0]
         # Remove the yaml extension and add .secrets.
         helm_secret_file = os.path.splitext(helm_secret_file)[0] + '.generated.yaml'
-        sys.stdout.write("Writing to: " + helm_secret_file + '\n')
         f = open(helm_secret_file, 'w')
         f.write(rendered_template)
         f.close()
-
-    def dict_from_tree(self, path):
-        keydata = dict()
-        for dirName, subdirList, fileList in os.walk(path):
-            print('Found directory: %s' % dirName)
-            keyname = dirName.split(self.keeval_bucket)[1].replace('/','.')
-            keyname = keyname.split(keeval_environment)[1].lstrip('.')
-            print keyname
-            for fname in fileList:
-                f = open(dirName + '/' + fname,"r")
-                data = f.read()
-                keydata[keyname] = data
-                print('\t%s' % fname)
-        print keydata
-
 
 # Argument parsing
 parser = argparse.ArgumentParser()
@@ -145,6 +114,7 @@ parser.add_argument('--deployment', help='deployment name', nargs=1, required=Tr
 parser.add_argument('--environment', help='project environment', nargs=1, required=True)
 parser.add_argument('--image', help='image name', nargs=1, required=True)
 parser.add_argument('--imagetag', help='image tag', nargs=1, required=True)
+parser.add_argument('--debug', help='Debug', required=False, action="store_true")
 
 args = parser.parse_args()
 
@@ -153,11 +123,11 @@ keeval_bucket = args.bucket[0]
 deployment = args.deployment[0]
 image = args.image[0]
 image_tag = args.imagetag[0]
-bucket_path = '/Users/jrhude/Documents/data/TelmedIQ/t/'
-config_path = [bucket_path, keeval_bucket, keeval_environment]
-config_path = '/'.join(config_path)
+DEBUG = args.debug
+config_path = keeval_environment
 
-builder = BuildSecrets(keeval_bucket=keeval_bucket)
+
+builder = j2Builder(keeval_bucket=keeval_bucket)
 
 # Look for AWS_PROFILE
 if 'AWS_PROFILE' in os.environ:
@@ -165,7 +135,32 @@ if 'AWS_PROFILE' in os.environ:
 else:
     aws_profile = None
 
+#Build the dictionary.
+generator = ConfigGenerator(
+    aws_profile,
+    keeval_bucket,
+    config_path + '/common',
+    config_path + '/deployment/' + deployment,
+    config_path + '/provisioning'
+
+)
+data = generator.generate()
+
+# Add some metadata to the dict.
+data['deployment'] = deployment
+data['environment'] = keeval_environment
+data['image'] = image
+data['imagetag'] = image_tag
+data['generatedtime'] = datetime.datetime.utcnow()
+# Loop through templates and render.
+if DEBUG is True:
+    pprint.pprint(data)
+
+
 # Get the j2 files in the current directory.
+
+cwd = getcwd()
+all_discovered_templates = []
 
 for root, dirnames, filenames in os.walk('.','secrets'):
     for filename in fnmatch.filter(filenames, '*.j2'):
@@ -175,23 +170,7 @@ if all_discovered_templates.__len__() is 0:
     sys.stderr.write("Could not find .j2 files. Nothing to do.\n")
     sys.exit(1)
 
-sys.stdout.write("Found: " + '\n'.join(all_discovered_templates) + '\n')
-
-#Build the dictionary.
-generator = ConfigGenerator(
-    config_path + '/common',
-    config_path + '/deployment/' + deployment,
-    config_path + '/provisioning/'
-)
-data = generator.generate()
-
-# Add some metadata to the dict.
-data['deployment'] = deployment
-data['environment'] = keeval_environment
-data['image'] = image
-data['imagetag'] = image_tag
-
-# Loop through templates and render.
+sys.stdout.write("Found templates:\n" + '\n'.join(all_discovered_templates) + '\n\n')
 
 for template in all_discovered_templates:
     rendered_template = builder.render(template, data)
